81a8f10af7bd44bb5d108f9f2bc3a253e7c672b5
Olga <olgayarsk@mail.ru>
2023-02-18 20:21:44 +0200

    Task

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.Clear();
+
+bool Pallindrom(int n)
+{     
+    int Nobr = 0, nn = n;
+    while (n > 0 || n < 0)
+    {
+        Nobr = Nobr * 10 + n % 10;
+        n = n / 10;
+    }
+    return Nobr == nn;
+}
+
+Console.Write("Введите число -> ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (Pallindrom(n))
+    Console.WriteLine("Палиндром");
+else
+    Console.WriteLine("Не палиндром");
+
+
+
--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.Clear();
+Console.Write("Введите число N  ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+int m = N;
+
+if (N < 0)
+{
+    i = N;
+    m = -N;
+    N = -1;
+}
+
+int[] result = new int[m];
+int j = 0;
+for (; i <= N; i++)
+{
+    result[j] = i;
+    j++;
+}
+for (j = 0; j < m; j++)
+{
+    Console.Write($"{Math.Pow(result[j], 3)}  ");
+}
